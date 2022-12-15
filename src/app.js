33880f23bda8b6d096c40f38ef57fac3e38c2919
Flydeer19 <114246352+Flydeer19@users.noreply.github.com>
2022-12-15 13:33:22 -0700

    implemented js for usecases without the score and the highscore

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
 "use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+document.querySelector("#secret-number").textContent = secretNumber;
+
+function showMessage(message) {
+  document.querySelector("#message").textContent = message;
+}
+
+function changeBackgroundColor(color) {
+  document.querySelector("body").style.backgroundColor = color;
+}
+
+function playGame() {
+  let inputValue = document.querySelector("#input-field").value;
+  let inputValueNumber = Number(inputValue);
+  const inputField = document.querySelector("#input-field");
+
+  if (inputValue === "") {
+    showMessage("😔 No number");
+  } else if (inputValueNumber < 1 || inputValueNumber > 20) {
+    alert(
+      `❗The number should be between 1 and 20❗ You entered ${inputValueNumber}`
+    );
+  } else if (inputValueNumber === secretNumber) {
+    showMessage("🏆You won");
+    changeBackgroundColor("#B1F44C");
+  } else {
+    let guessRange =
+      inputValueNumber > secretNumber ? "🡵 Too high" : "🡶 Too low";
+    showMessage(guessRange);
+  }
+  inputField.focus();
+  document.querySelector("#input-field").value = "";
+}
+let checkButton = document.querySelector("#check-button");
+checkButton.addEventListener("click", playGame);
